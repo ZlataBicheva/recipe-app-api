25be4609785576f87b72599e7fdb973ed7f083ac
ZlataBicheva <zlatabiceva@gmail.com>
2024-10-07 21:31:26 +0200

    User API update

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from drf_spectaculas.views import (
+from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
@@ -24,7 +24,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectaculasAPIView.as_view(), name='api-schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
